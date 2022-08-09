fe7b411bf321e6092a5ed55bc9b061ce5cc8463c
pdrego <pdrego@sfu.ca>
2022-08-09 15:14:55 -0700

    Created a working button that could act as our delegator lock/unlock button

--- swivel-app/App.js
+++ swivel-app/App.js
@@ -1,14 +1,42 @@
+import React, { useState } from 'react';
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { Text, View, StyleSheet, Button, Alert, TouchableOpacity } from 'react-native';
 
 export default function App() {
+  const [delegatorStatus = 'LOCKED', setStatus] = useState('LOCKED');
+  
   return (
     <View style={styles.container}>
-      <Text>Swivel Demo App</Text>
+      <Text>Swivel App</Text>
       <StatusBar style="auto" />
+
+      {/* Adds button to unlock Delegator */}
+      <Text style={[styles.title, styles.setColor]}>
+        Please press this button to unlock the delegator
+      </Text>
+
+      {/* This is the button */}      
+      <TouchableOpacity
+        style={[styles.delegator_button]}
+        onPress={buttonPress}
+        // onPress={() => setStatus('UNLOCKED')}
+      >
+      <Text style={[styles.title, styles.setColorGreen]}>
+        Delegator {delegatorStatus} 
+      </Text>
+      </TouchableOpacity>
+
     </View>
+
   );
 }
+
+
+
+const buttonPress = () => {
+  alert('Delegator Unlocked ')
+  // Enter code to unlock delegator
+};
 
 const styles = StyleSheet.create({
   container: {
@@ -17,4 +45,17 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+  setColorGreen : {
+    color: '#39FF14'
+  },
+  setColorRed : {
+    color: '#19FF14'
+  },
+
+  delegator_button: {
+    alignItems: "center",
+    backgroundColor: "#000000",
+    padding: 10
+  },
+
 });
