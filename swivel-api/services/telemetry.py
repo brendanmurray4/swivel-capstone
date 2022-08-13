0a47fa7c01e1dc11b1bd4cbd884f602a0170d062
bjmurray <bjmurray@sfu.ca>
2022-08-12 17:31:20 -0700

    Converted to sqlite 3 from pyodbc for local database

--- swivel-api/services/telemetry.py
+++ swivel-api/services/telemetry.py
@@ -1,31 +1,40 @@
-from Flask import Blueprint, request, jsonify, g
+from flask import Blueprint, request, jsonify, g
+import sqlite3
 from requests import ResponseSuccess
-import pyodbc
 import json
 from connect_db import get_db
-
+#Change this path to somewhere near root?
+DB_PATH = "swivel.db"
 StatusService = Blueprint("status_service", __name__)
+# GET /telemetry/<DEVICE_ID>/
+# POST /telemetry/<DEVICE_ID>/
+# GET /state/current/<DEVICE_ID>/
+# POST /state/<DEVICE_ID>/
+ 
 
-@app.teardown_appcontext
+@StatusService.teardown_appcontext
 def close_db(error):
     """Closes the database again at the end of the request."""
     if hasattr(g, 'azure_db'):
         g.azure_db.close()
 
+
 @StatusService.route("/telemetry/<device_id>", methods=['GET', 'POST'])
 def telemetry(device_id):
     if request.method == 'POST':
-        conn = get_db()
-        data = request.json
-        data = json.load(data)
-        conn.autocommit = False
-        conn.execute("DELETE FROM DeviceTelemetry WHERE id = ?", device_id)
-        conn.execute("INSERT INTO DeviceTelemetry(id, latitude, longitude, acceleration) values (?, ?, ?, ?)", tuple(data.items))
-        conn.autocommit = True
+        conn = sqlite3.connect(DB_PATH)
+        cursor = conn.cursor()
+        reqdata = request.json
+        reqdata = json.load(reqdata)
+        cursor.execute("BEGIN TRANSACTION")
+        cursor.execute("DELETE FROM DeviceTelemetry WHERE id = ?", device_id)
+        cursor.execute("INSERT INTO DeviceTelemetry(id, latitude, longitude, acceleration) values (?, ?, ?, ?)", tuple(reqdata.items))
+        cursor.execute("COMMIT TRANSACTION")
         resp = ResponseSuccess({"Success": "POST"})
     if request.method == 'GET':
-        conn = get_db()
-        cursor = conn.execute("SELECT * FROM DeviceTelemetry WHERE id = ?", device_id)
-        records = cursor.fetchall()
+        conn = sqlite3.connect(DB_PATH)
+        cursor = conn.cursor()
+        records = cursor.execute("SELECT * FROM DeviceTelemetry WHERE id = ?", device_id)
         resp = ResponseSuccess(records[0])
+    
     return resp.encode_json()
