b5010ff7f4b0986bbc75bd19050a469268c777de
pdrego <pdrego@sfu.ca>
2022-08-12 21:00:25 -0700

    magic max

--- swivel-app/GetHttp.js
+++ swivel-app/GetHttp.js
@@ -3,16 +3,18 @@ import { ActivityIndicator, FlatList, Text, View } from 'react-native';
 
 export function gethttp() {
   const [isLoading, setLoading] = useState(true);
-  const [data, setData] = useState([]);
-  const [data2, setData2] = useState([]);
+  const [data, setData] = useState(null);
+  // const [data2, setData2] = useState([]);
+  
 
   const getDataFromServer = async () => {
     try {
-      const response = await fetch('https://reactnative.dev/movies.json');
+      const response = await fetch('https://iot.swivel.bike/status/ping');
       // const response = await fetch('https://iot.swivel.bike/status/ping');
       const json = await response.json();
-      setData(json.movies); // From the JSON file we use the "movies header"
-      setData2(json.description); // From the JSON file we use the "description header"
+      setData(json); // From the JSON file we use the "movies header"
+      console.log(json)
+      // setData2(json.description); // From the JSON file we use the "description header"
 
     } catch (error) { // Prints error if one occurs
       console.error(error);
@@ -27,21 +29,10 @@ export function gethttp() {
 
   return (
     <View style={{ flex: 1, padding: 32 }}>
-      {isLoading ? (
-        <ActivityIndicator />
-      ) : (
-        <FlatList
-          data={data}
-          keyExtractor={({ id }, index) => id}
-          renderItem={({ item }) => ( // Renders item into a list
             <Text>
-              {item.title}, {item.releaseYear}, {data2}
+              {data && data.data.message}
             </Text>
-          )}
-        />
-      )}
+
     </View>
   );
 }
-
-
