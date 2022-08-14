b4b6258560829bd324176b65e4b44fc0474ee414
pdrego <pdrego@sfu.ca>
2022-08-13 18:21:51 -0700

    GetHttps() returns longitude and latitude variables

--- swivel-app/App.js
+++ swivel-app/App.js
@@ -15,83 +15,25 @@ import {
 import LoginPage from './LoginPage';
 import { MapPage } from './MapPage';
 import { DelegatorPage } from './DelegatorPage';
-import { gethttp } from './GetHttp';
+
 
 export default function App() {
-  
-
   return (
-    gethttp()
-    // // NAVIGATE BETWEEN PAGES
-    // <NavigationContainer>
-    //   <MyStack />
-    // </NavigationContainer>
-
-  //   <View style={styles.container}>
-  //   <Text>Swivel Delegator Page</Text>
-  //   <StatusBar style="auto" />
-
-  //   <Text style={[styles.title, styles.setColor]}>
-  //     Please press this button to unlock the delegator
-  //   </Text>
-
-  //   {/* This is the delegator button */}
-  //   <TouchableOpacity style={styles.delegator_button}>
-  //     <Text style={[styles.title, styles.setColorGreen]}>Unlock Delegator</Text>
-  //   </TouchableOpacity>
-
-  //   {/* BACK BUTTON */}
-  //   <TouchableOpacity style={styles.back_button} onPress={() => navigation.goBack()}>
-  //     <Text style={[styles.title, styles.setColorWhite]}>Back</Text>
-  //   </TouchableOpacity>
-  // </View>
+    // NAVIGATE BETWEEN PAGES
+    <NavigationContainer>
+      <MyStack />
+    </NavigationContainer>
   );
 }
 
-// /* NAVIGATE BETWEEN PAGES */
-// const Stack = createNativeStackNavigator();
-// function MyStack() {
-//   return (
-//     <Stack.Navigator screenOptions={{ headerShown: false }}>
-//       <Stack.Screen name="Login" component={LoginPage} />
-//       <Stack.Screen name="Map" component={MapPage} />
-//       <Stack.Screen name="Delegator" component={DelegatorPage} />
-//     </Stack.Navigator>
-//   );
-// }
-
-// import React, { useEffect, useState } from 'react';
-// import { FlatList, Text, View } from 'react-native';
-
-// export default App = () => {
-//   const [isLoading, setLoading] = useState(true);
-//   const [data, setData] = useState([]);
-//   console.log(data);
-
-//   useEffect(() => {
-//     fetch('https://raw.githubusercontent.com/adhithiravi/React-Hooks-Examples/master/testAPI.json')
-//       .then((response) => response.json())
-//       .then((json) => setData(json))
-//       .catch((error) => console.error(error))
-//       .finally(() => setLoading(false));
-//   }, []);
-
-//   return (
-
-//     <View style={{ flex: 1, padding: 24 }}>
-//       {isLoading ? <Text>Loading...</Text> : 
-//       ( <View style={{ flex: 1, flexDirection: 'column', justifyContent:  'space-between'}}>
-//           <Text style={{ fontSize: 18, color: 'green', textAlign: 'center'}}>{data.title}</Text>
-//           <Text style={{ fontSize: 14, color: 'green', textAlign: 'center', paddingBottom: 10}}>Articles:</Text>
-//           <FlatList
-//             data={data.articles}
-//             keyExtractor={({ id }, index) => id}
-//             renderItem={({ item }) => (
-//               <Text>{item.id + '. ' + item.title}</Text>
-//             )}
-//           />
-//         </View>
-//       )}
-//     </View>
-//   );
-// };
+/* NAVIGATE BETWEEN PAGES */
+const Stack = createNativeStackNavigator();
+function MyStack() {
+  return (
+    <Stack.Navigator screenOptions={{ headerShown: false }}>
+      <Stack.Screen name="Login" component={LoginPage} />
+      <Stack.Screen name="Map" component={MapPage} />
+      <Stack.Screen name="Delegator" component={DelegatorPage} />
+    </Stack.Navigator>
+  );
+}
--- swivel-app/GetHttp.js
+++ swivel-app/GetHttp.js
@@ -1,21 +1,24 @@
 import React, { useEffect, useState } from 'react';
 import { ActivityIndicator, FlatList, Text, View } from 'react-native';
 
+// Function returns [latitude, longitude], two variables based on destination address
 export function gethttp() {
   const [isLoading, setLoading] = useState(true);
-  const [data, setData] = useState(null);
+  var [latitude, setLatitude] = useState(null);
+  var [longitude, setLongitude] = useState(null);
 
   const getDataFromServer = async () => {
     try {
       const response = await fetch('https://iot.swivel.bike/status/ping');
       const json = await response.json();
-      setData(json); // From the JSON file we use the "movies header"
+      setLatitude(json.data.message); // replace with .latitude
+      setLongitude(json.data.message); // replace with .longitude
       console.log(json);
     } catch (error) {
       // Prints error if one occurs
       console.error(error);
     } finally {
-      // We are no longer loading, the json file ahs loaded
+      // We are no longer loading, the json file has loaded
       setLoading(false);
     }
   };
@@ -25,9 +28,5 @@ export function gethttp() {
     getDataFromServer();
   }, []);
 
-  return (
-    <View style={{ flex: 1, padding: 32 }}>
-      <Text>{data && data.data.message}</Text>
-    </View>
-  );
+  return [latitude, longitude];
 }
