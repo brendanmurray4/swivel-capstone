949bf18b803e61c12651ae4dcba9b03bebda3c49
pdrego <pdrego@sfu.ca>
2022-10-05 13:33:13 -0700

    Added new purchase page to mirror the prototype document

--- swivel-app/App.js
+++ swivel-app/App.js
@@ -13,6 +13,7 @@ import {
   Modal,
   Pressable,
   Alert,
+  ScrollView,
 } from 'react-native';
 import MapView, { PROVIDER_GOOGLE, Marker } from 'react-native-maps';
 
@@ -30,10 +31,75 @@ const Stack = createNativeStackNavigator();
 function MyStack() {
   return (
     <Stack.Navigator screenOptions={{ headerShown: false }} defaultScreenOptions={DelegatorPage}>
+      <Stack.Screen name="Purchase Page" component={PurchasePage} />
       <Stack.Screen name="Delegator" component={DelegatorPage} />
       <Stack.Screen name="Login" component={LoginPage} />
       <Stack.Screen name="Map" component={MapPage} />
     </Stack.Navigator>
+  );
+}
+
+/* INITIAL SIGN IN PAGE */
+function PurchasePage({ navigation }) {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+
+  return (
+    <View style={purchaseStyles.container}>
+      <View style={purchaseStyles.top} >
+      <Image style={{resizeMode: "cover", height: "100%", width: "100%"}} source={require('./assets/swivel_bike.png')} />
+      </View>
+
+
+      <View style={purchaseStyles.middle} >
+        <Text style={{fontWeight: 'bold', textAlign: 'center', fontSize: 26}}> Emonda SLR 9 eTap</Text>
+        <Text style={purchaseStyles.priceText}> $ 6.55
+          <Text style={{fontWeight: 'bold', color: '#BFC0BD', fontSize: 14, textAlign: 'center'}}> /Hour</Text>
+        </Text>
+      </View>
+
+      <View style={purchaseStyles.middle2} >
+      <Image style={{resizeMode: "cover", height: "100%", width: "20%", resizeMode: 'contain', aspectRatio: 0.75, left: "2%"}} source={require('./assets/gps_icon.jpg')} />
+        <Text style={{fontWeight: 'bold', color: '#BFC0BD', fontSize: 22, textAlign: 'center', position: 'absolute', left: '20%', flexShrink: 1, }}> Science Rd, Burnaby, BC, Canada </Text>
+
+
+      </View>
+      <View style={purchaseStyles.middle2} >
+
+      <Image style={{resizeMode: "cover", height: "100%", width: "20%", resizeMode: 'contain'}} source={require('./assets/clock_icon.jpg')} />
+        <Text style={{fontWeight: 'bold', color: '#BFC0BD', fontSize: 22, textAlign: 'center', position: 'absolute', left: '20%', flexShrink: 1, flexWrap: 'wrap'}}> 
+        Sept 20th, 8:00 AM to Sept 20th, 11:00 AM </Text>
+      </View>
+
+      
+      <View style={purchaseStyles.bottom} >
+      <ScrollView>
+        <View style={purchaseStyles.bottom2} >
+            <Text style={{fontWeight: 'bold', color: '#000000', fontSize: 18, textAlign: 'left', fontWeight: 'bold'}}> Payment Summary</Text>
+            <Text style={{fontWeight: 'bold', color: '#000000', fontSize: 18, textAlign: 'right', fontWeight: 'bold'}}> $ 24.01</Text>
+        </View>
+        <View style={purchaseStyles.bottom2} >
+            <Text style={{fontWeight: 'bold', color: '#BFC0BD', fontSize: 18, textAlign: 'left', fontWeight: 'bold'}}> Hourly Cost</Text>
+            <Text style={{fontWeight: 'bold', color: '#BFC0BD', fontSize: 18, textAlign: 'right', fontWeight: 'bold'}}> $ 19.65</Text>
+        </View>
+        <View style={purchaseStyles.bottom2} >
+            <Text style={{fontWeight: 'bold', color: '#BFC0BD', fontSize: 18, textAlign: 'left', fontWeight: 'bold'}}> GST + PST</Text>
+            <Text style={{fontWeight: 'bold', color: '#BFC0BD', fontSize: 18, textAlign: 'right', fontWeight: 'bold'}}> $ 2.36</Text>
+        </View>
+        <View style={purchaseStyles.bottom2} >
+            <Text style={{fontWeight: 'bold', color: '#000000', fontSize: 18, textAlign: 'center', fontWeight: 'bold'}}> This is Scrollable...</Text>
+        </View>
+        </ScrollView>
+      </View>
+      
+
+      <View style={purchaseStyles.confirmButton} >
+        <TouchableOpacity style={styles.confirmButton} onPress={() => navigation.navigate('FinalPurchase')}>
+          <Text style={{fontWeight: 'bold', color: '#000000', fontSize: 26, textAlign: 'left', fontWeight: 'bold'}}> Confirm </Text>
+        </TouchableOpacity>
+      </View>
+
+    </View>
   );
 }
 
@@ -315,16 +381,95 @@ export function DelegatorPage({ navigation }) {
     </View>
   );
 }
+
+
+/* DEFAULT STYLES */
+const purchaseStyles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: "space-between",
+    backgroundColor: "#fff",
+    // padding: 20,
+    margin: 0,
+  },
+  top: {
+    flex: 0.40,
+    backgroundColor: "grey",
+    borderWidth: 1,
+    // borderTopLeftRadius: 20,
+    // borderTopRightRadius: 20,
+  },
+  middle: {
+    flex: 0.1,
+    // backgroundColor: "beige",
+    borderWidth: 1,
+  },
+  middle2: {
+    flex: 0.1,
+    // backgroundColor: "beige",
+    borderWidth: 1,
+  },
+  bottom: {
+    flex: 0.25,
+    // flexDirection: 'row',
+    // justifyContent: 'space-between',
+    // backgroundColor: "pink",
+    borderWidth: 1,
+    borderBottomLeftRadius: 20,
+    borderBottomRightRadius: 20,
+  },
+  bottom2: {
+    // flex: 0.2,
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    // backgroundColor: "pink",
+    borderWidth: 1,
+    borderBottomLeftRadius: 20,
+    borderBottomRightRadius: 20,
+  },
+
+  confirmButton: {
+    flex: 0.05,
+    backgroundColor: '#B4FF39',
+    color: '#000',
+    textAlign: 'center',
+    display: 'flex',
+    justifyContent: 'center',
+    alignItems: 'center',
+    paddingTop: 15,
+    paddingBottom: 15,
+    // marginTop: 25,
+    marginBottom: 15,
+    borderBottomLeftRadius: 20,
+    borderBottomRightRadius: 20,
+  },
+
+
+  baseText: {
+    // fontSize: 14,
+    textAlign: 'center',
+    justifyContent: 'flex-end',
+  },
+
+  priceText: {
+    fontSize: 26,
+    textAlign: 'center',
+    justifyContent: 'flex-end',
+    color: '#B4FF39',
+  },
+
+
+});
+
+
 /* DEFAULT STYLES */
 const styles = StyleSheet.create({
   container: {
-    display: 'flex',
     flex: 1,
-    backgroundColor: '#e2e8f0',
-    alignItems: 'center',
-    justifyContent: 'flex-end',
-    width: '100%',
-    flexDirection: 'column',
+    justifyContent: "space-between",
+    backgroundColor: "#fff",
+    padding: 20,
+    margin: 10,
   },
 
   dataContainer: {
