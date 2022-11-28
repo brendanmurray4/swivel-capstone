489db521e4885c198f5d95bc09d6e5a8c87525a2
bjmurray <bjmurray@sfu.ca>
2022-11-28 15:58:22 -0800

    Added testing for helium endpoint

--- swivel-api/main.py
+++ swivel-api/main.py
@@ -9,11 +9,13 @@ from flask import Flask
 from services.status import StatusService
 from services.telemetry import TelemetryService
 from services.control import ControlService
+from services.helium import HeliumService
 
 app = Flask(__name__)
 app.register_blueprint(StatusService, url_prefix="/status")
 app.register_blueprint(TelemetryService, url_prefix="/telemetry")
 app.register_blueprint(ControlService, url_prefix = "/control")
+app.register_blueprint(HeliumService, url_prefix = "/helium")
 
 if __name__ == "__main__":
     app.run(host=os.environ.get("API_HOST"), port=os.environ.get("API_PORT"))
---
+++ swivel-api/services/helium.py
@@ -0,0 +1,15 @@
+
+import json
+import datetime
+from xmlrpc.client import ResponseError
+from flask import Blueprint, request, jsonify
+from requests import ResponseSuccess
+from cache import cache
+
+HeliumService = Blueprint("helium_service", __name__)
+
+@HeliumService.route("/device", methods=["GET", "POST"])
+def device():
+    if request.method =="POST":
+        reqdata=request.json
+        print(reqdata)
