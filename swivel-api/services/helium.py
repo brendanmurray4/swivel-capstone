3a7854c37da08b411c94dfeb4a4ffa193be178f2
bjmurray <bjmurray@sfu.ca>
2022-11-28 22:51:15 -0800

    Added route logic for join vs gps data post requests

--- swivel-api/services/helium.py
+++ swivel-api/services/helium.py
@@ -1,6 +1,8 @@
-
+import regex
 import json
 import datetime
+import base64
+import requests
 from xmlrpc.client import ResponseError
 from flask import Blueprint, request, jsonify
 from requests import ResponseSuccess
@@ -11,11 +13,23 @@ HeliumService = Blueprint("helium_service", __name__)
 @HeliumService.route("/device", methods=["GET", "POST"])
 def device():
     if request.method =="POST":
-        reqdata=request.json
-        print(reqdata)
-        resp = ResponseSuccess(reqdata)
+        reqdata=request.data
+        stringdata = str(reqdata)
+        datadict = json.loads(stringdata)
+        if datadict['type'] == 'join':
+            #resp = ResponseSuccess(cache.telemetrycache[0])
+            return ResponseSuccess(cache.telemetrycache[0]["state"]).encode_json()
+        elif datadict['type'] == 'uplink':
+            #GPS data
+            stringdata = str(reqdata)
+            data = stringdata.split(';')
+            cache.telemetrycache[0] = {"location":data[0], "state":data[1],"battery":data[2]}
+            resp = ResponseSuccess(data)
     if request.method =="GET":
-        reqdata=request.json
-        print(reqdata)
-        resp = ResponseSuccess(reqdata)
+        stringdata = str(request.data)
+        datadict = json.loads(stringdata)
     return resp.encode_json()
+
+def sendtodevice(integrationid, downlinkkey, device_id, jsonbody):
+    url = "https://console.helium.com/api/v1/down" + "/" + str(integrationid) + "/" + str(downlinkkey) + "/" + str(device_id)
+    requests.post(url, json = jsonbody)
